7adbd953fd18e54523af11e5e2c4ac042806c6b3
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-11-25 11:23:59 +0600

    Poduct schema & model created

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,20 @@
+import { Schema, model } from 'mongoose';
+import { TProduct } from './product.interface';
+
+/* This code snippet is defining a Mongoose schema for a product entity. Let's break it down: */
+const productSchema = new Schema<TProduct>({
+  name: { type: String, required: true },
+  brand: { type: String, required: true },
+  price: { type: Number, required: true },
+  type: {
+    type: String,
+    enum: ['Mountain', 'Road', 'Hybrid', 'BMX', 'Electric'],
+    required: true,
+  },
+  description: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  inStock: { type: Boolean, required: true },
+});
+
+// Create a Model
+export const Product = model<TProduct>('Product', productSchema);
