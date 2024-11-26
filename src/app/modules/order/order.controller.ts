304b219ae938554b08c02eff2da305cf24e49e1f
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-11-27 00:13:14 +0600

    total revenue in the order controller file

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -28,6 +28,27 @@ const createOrder = async (req: Request, res: Response) => {
   }
 };
 
+const calculateRevenue = async (req: Request, res: Response) => {
+  try {
+    const result = await OrderServices.calculateTotalRevenue();
+
+    // send the response to the client
+    res.status(200).json({
+      success: true,
+      message: 'Revenue calculated successfully',
+      data: result,
+    });
+  } catch (err) {
+    const error = err as Error;
+    res.status(500).json({
+      success: false,
+      message: error.message || 'Something went wrong',
+      data: error,
+    });
+  }
+};
+
 export const OrderControllers = {
   createOrder,
+  calculateRevenue,
 };
