1d81531d5a75e12327f090d443ec81a4ab4abbbe
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-11-26 21:44:16 +0600

    order model added

---
+++ src/app/modules/order/order.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, model } from 'mongoose';
+import { TOrder } from './order.interface';
+
+/* This code snippet is defining a Mongoose schema for an order document in a MongoDB database. Let's
+break down what each part of the schema is doing: */
+const orderSchema = new Schema<TOrder>({
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: [true, 'Product reference is required'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'Quantity is required'],
+  },
+  totalPrice: {
+    type: Number,
+    required: [true, 'Total price is required'],
+  },
+});
+
+// Create a model
+export const Order = model<TOrder>('Order', orderSchema);
