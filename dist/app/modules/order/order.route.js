00d36f64c25f176f01d51f486807a3d7936b3e51
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-12-14 09:17:55 +0600

    updated route checked with new valuyes

--- dist/app/modules/order/order.route.js
+++ dist/app/modules/order/order.route.js
@@ -7,6 +7,6 @@ exports.OrderRoutes = void 0;
 const express_1 = __importDefault(require("express"));
 const order_controller_1 = require("./order.controller");
 const router = express_1.default.Router();
-router.post('/createOrder', order_controller_1.OrderControllers.createOrder);
+router.post('/', order_controller_1.OrderControllers.createOrder);
 router.get('/revenue', order_controller_1.OrderControllers.calculateRevenue);
 exports.OrderRoutes = router;
