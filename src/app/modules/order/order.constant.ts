e2fcd46a794a9196a96883e993f35cb18eb01e0d
Nadim-Nion <nadim15-1746@diu.edu.bd>
2026-05-04 19:10:30 +0600

    order interface and models added

---
+++ src/app/modules/order/order.constant.ts
@@ -0,0 +1,7 @@
+export const ORDER_STATUS_ARR = [
+  'pending',
+  'paid',
+  'shipped',
+  'completed',
+  'cancelled',
+] as const;
--- src/app/modules/order/order.model.ts
+++ src/app/modules/order/order.model.ts
@@ -1,28 +1,42 @@
 import { Schema, model } from 'mongoose';
+import { ORDER_STATUS_ARR } from './order.constant';
 import { TOrder } from './order.interface';
 
 /* This code snippet is defining a Mongoose schema for an order document in a MongoDB database. Let's
 break down what each part of the schema is doing: */
 const orderSchema = new Schema<TOrder>(
   {
-    email: {
-      type: String,
-      required: [true, 'Email is required'],
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'User reference is required'],
     },
+
     product: {
-      type: String,
+      type: Schema.Types.ObjectId,
       ref: 'Product',
       required: [true, 'Product reference is required'],
     },
+
     quantity: {
       type: Number,
       required: [true, 'Quantity is required'],
-      min: [0, 'Quantity must be a positive number.'],
+      min: [1, 'Quantity must be at least 1'],
     },
+
     totalPrice: {
       type: Number,
       required: [true, 'Total price is required'],
       min: [0, 'Total price must be a positive number'],
+    },
+
+    status: {
+      type: String,
+      enum: {
+        values: ORDER_STATUS_ARR,
+        message: `{VALUE} is not a valid status. Status must be one of the following: ${ORDER_STATUS_ARR.join(', ')}`,
+      },
+      default: 'pending',
     },
   },
   {
